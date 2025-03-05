4216fb421bab402ad1bc5548c9dc635d74830278
Your Name <you@example.com>
2025-03-05 23:37:20 +0200

    first commit

--- components/header/nav/index.jsx
+++ components/header/nav/index.jsx
@@ -31,9 +31,7 @@ export default function index() {
     <motion.div variants={menuSlide} initial="initial" animate="enter" exit="exit" className={styles.menu}>
        <div className={styles.body}>
             <div onMouseLeave={() => {setSelectedIndicator(pathname)}} className={styles.nav}>
-                    <div className={styles.header}>
-                        <p>Navigation</p>
-                    </div>
+                     
                     {
                       navItems.map( (data, index) => {
                         return <Link key={index} data={{...data, index}} isActive={selectedIndicator == data.href} setSelectedIndicator={setSelectedIndicator}></Link>
