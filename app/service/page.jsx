b8faa1d7cc8a8b637bb43a120082054dad0c6af8
Your Name <you@example.com>
2025-03-18 15:38:28 +0200

    first commit

--- app/service/page.jsx
+++ app/service/page.jsx
@@ -6,6 +6,7 @@ import About1 from "../../components/About1";
 import Swip from "../../components/Swip";
 import Swip3 from "../../components/Swip3";
 import Services1 from "../../components/Services1";
+import { Suspense } from 'react';
 
 export default function Home() {
 
@@ -17,14 +18,14 @@ export default function Home() {
     <>
 
 
-
+<Suspense >
       <Carousel1 />
       <About1 />
       <Swip />
       <Services1 />
       <Swip3 /> 
       <Footer />
-
+      </Suspense>
  
 
     </>
