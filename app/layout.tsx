9e590ae328f703053873cbedb958f9964f78d5d8
Your Name <you@example.com>
2025-03-20 22:40:00 +0200

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -299,8 +299,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Logo />
-        <Header />
+        <Logo /> 
         {children}
       </body>
     </html>
